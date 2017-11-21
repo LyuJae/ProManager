8c8f59fcde1f807fbd3abbb02730922dc4c31e07
LyuJae <rjch221@naver.com>
2017-11-21 23:09:32 +0900

    down and commit test

--- app/src/main/java/com/example/ryu/promanager/MainActivity.java
+++ app/src/main/java/com/example/ryu/promanager/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.ryu.promanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
