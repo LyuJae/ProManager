19aae3d4d5c8aae9862285d2ffe23833e0b3a90f
TAEHWANCHA <ckxoghksdlek@naver.com>
2017-11-24 14:03:04 +0900

    test commit3_1

--- app/src/main/java/com/example/ryu/promanager/MainActivity.java
+++ app/src/main/java/com/example/ryu/promanager/MainActivity.java
@@ -1,49 +1,44 @@
 package com.example.ryu.promanager;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
 public class MainActivity extends AppCompatActivity {
-    TextView textName, textProject, textProgress, textMajor, textStu_number
-    Button Btnadd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Intent intent = getIntent();
-        textName = (TextView) findViewById(R.id.textName);
-        textMajor = (TextView) findViewById(R.id.major)
-        textProject = (TextView) findViewById(R.id.textProject);
-        textProgress = (TextView) findViewById(R.id.textProgress);
-        Btnadd = (Button) findViewById(R.id.btnadd);
+        setContentView(R.layout.firstadd);
 
-        String name = intent.getStringExtra("contact_name");
-        if(name !=null)
-            textName.setText(name);
-        String Major = intent.getStringExtra("contact_major");
-        if(name !=null)
-            textName.setText(name);
-        String Number = intent.getStringExtra("contact_number");
-        if(name !=null)
-            textName.setText(name);
-        String proj = intent.getStringExtra("contact_project");
-        if(proj !=null)
-            textProject.setText(proj);
-        String prog = intent.getStringExtra("contact_progress");
-        if(proj !=null)
-            textProgress.setText(prog);
+       Button Btnsave;
 
-        Btnadd.setOnClickListener(new View.OnClickListener() {
+        Btnsave = (Button) findViewById(R.id.btnSave);
+
+        Btnsave.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intents = new Intent(getApplicationContext(),addAct.class);
-                startActivity(intents);
+        Intent intent = new Intent(MainActivity.this, RealActivity.class);
 
+        EditText firstName = (EditText) findViewById(R.id.firstname);
+        intent.putExtra("contact_name", firstName.getText().toString());
+        EditText Major = (EditText) findViewById(R.id.major);
+        intent.putExtra("contact_major", Major.getText().toString());
+        EditText Stu_Number = (EditText) findViewById(R.id.Stu_number);
+        intent.putExtra("contact_number", Stu_Number.getText().toString());
+        EditText firstProject = (EditText) findViewById(R.id.firstproject);
+        intent.putExtra("contact_project", firstProject.getText().toString());
+        EditText firstProgress = (EditText) findViewById(R.id.firstprogress);
+        intent.putExtra("contact_progress", firstProgress.getText().toString());
+
+
+                startActivity(intent);
             }
         });
+
+
+
     }
 }
--- app/src/main/java/com/example/ryu/promanager/RealActivity.java
+++ app/src/main/java/com/example/ryu/promanager/RealActivity.java
@@ -7,8 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textName, textProject, textProgress, textMajor, textStu_number
+
+
+public class RealActivity extends AppCompatActivity {
+
     Button Btnadd;
 
     @Override
@@ -16,24 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Intent intent = getIntent();
-        textName = (TextView) findViewById(R.id.textName);
-        textMajor = (TextView) findViewById(R.id.major)
-        textProject = (TextView) findViewById(R.id.textProject);
-        textProgress = (TextView) findViewById(R.id.textProgress);
+
         Btnadd = (Button) findViewById(R.id.btnadd);
 
+        TextView textname = (TextView) findViewById(R.id.textName);
         String name = intent.getStringExtra("contact_name");
         if(name !=null)
-            textName.setText(name);
+            textname.setText(name);
+        TextView textMajor = (TextView) findViewById(R.id.textmajor);
         String Major = intent.getStringExtra("contact_major");
         if(name !=null)
-            textName.setText(name);
+            textMajor.setText(Major);
+        TextView textStu_number = (TextView) findViewById(R.id.textstu_number);
         String Number = intent.getStringExtra("contact_number");
         if(name !=null)
-            textName.setText(name);
+            textStu_number.setText(Number);
+        TextView textProject = (TextView) findViewById(R.id.textproject);
         String proj = intent.getStringExtra("contact_project");
         if(proj !=null)
             textProject.setText(proj);
+        TextView textProgress = (TextView) findViewById(R.id.textprogress);
         String prog = intent.getStringExtra("contact_progress");
         if(proj !=null)
             textProgress.setText(prog);
